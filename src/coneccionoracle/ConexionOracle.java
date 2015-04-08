6a64a37f36eab71e86c7d31adefcef33b186f2a8
alumno04 <alumno04@TPP-LAB04-03.TPP.UPEU.EDU.PE>
2015-04-08 11:15:45 -0500

    coneccion con la base de datos..

--- src/coneccionoracle/ConexionOracle.java
+++ src/coneccionoracle/ConexionOracle.java
@@ -1,22 +1,37 @@
 
 package coneccionoracle;
 
-/**
- *
- * @author alumno04
- */
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+
 public class ConexionOracle {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         
         if (Coneccion.conectar() ==null) {
             
             System.out.println("La coneccion falló!!!.");
-        } else {
+        } 
+        else {
             System.out.println("La coneccion fue Exitosa :)");
+            try {
+                 Statement st =Coneccion.conectar().createStatement();
+                 ResultSet rs=st.executeQuery("SELECT * FROM persona");
+                 while (rs.next()) {
+                     System.out.println("ID:"+rs.getInt("IDPERSONA"));
+                     System.out.println("1:"+rs.getString("NOMBRE"));
+                     System.out.println("2:"+rs.getString("APELLIDO_PAT"));
+                     System.out.println("3:"+rs.getString("APELLIDO_MAT"));
+                     System.out.println("4:"+rs.getString("DNI"));
+                    
+                }
+            } 
+            catch (Exception e) {
+                
+            }
+           
         }
   
     }
